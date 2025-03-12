cb2f0bf33013128e9386353060e616c9a042d621
chloedao <chloedaoo@gmail.com>
2025-03-12 15:05:57 +0100

    update 15:05

--- Assets/Script/ARController.cs
+++ Assets/Script/ARController.cs
@@ -1,12 +1,12 @@
-/*using UnityEngine;
+using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using System.Collections.Generic;
 
 public class ARController : MonoBehaviour
 {
     public ARPlaneManager arPlaneManager;
-    public GameObject fishPrefab; // Dra in din fisk-prefab här
-    public float fishSpawnDepth = 0.3f; // Avstånd under vattenytan
+    public GameObject fishPrefab; // Dra in din fisk-prefab hï¿½r
+    public float fishSpawnDepth = 0.3f; // Avstï¿½nd under vattenytan
     public int fishCount = 5; // Antal fiskar per yta
 
     void OnEnable()
@@ -21,7 +21,7 @@ public class ARController : MonoBehaviour
 
     void OnPlanesChanged(ARPlanesChangedEventArgs args)
     {
-        foreach (ARPlane plane in args.added) // När en ny yta upptäcks
+        foreach (ARPlane plane in args.added) // Nï¿½r en ny yta upptï¿½cks
         {
             SpawnFishUnderPlane(plane);
         }
@@ -35,72 +35,10 @@ public class ARController : MonoBehaviour
             spawnPosition.y -= fishSpawnDepth; // Placera under ytan
 
             GameObject fish = Instantiate(fishPrefab, spawnPosition, Quaternion.identity);
-            fish.transform.SetParent(plane.transform); // Fäst på ARPlanet
+            fish.transform.SetParent(plane.transform); // Fï¿½st pï¿½ ARPlanet
         }
     }
 }
-*/
 
 
-using UnityEngine;
-using UnityEngine.XR.ARFoundation;
 
-public class ARController : MonoBehaviour
-{
-    public DrivingSurfaceManager drivingSurfaceManager; // Hämtar ytan
-    public GameObject waterPrefab; // Prefab för vatten
-    private GameObject waterInstance;
-    private Transform mainPlane;
-
-    void Start()
-    {
-        if (drivingSurfaceManager == null)
-        {
-            Debug.LogError("DrivingSurfaceManager är inte tilldelad i ARController!");
-            return;
-        }
-    }
-
-    void Update()
-    {
-        // Hämta den "Locked Plane" som DrivingSurfaceManager använder
-        if (drivingSurfaceManager.LockedPlane != null)
-        {
-            Transform lockedTransform = drivingSurfaceManager.LockedPlane.transform;
-
-            if (waterInstance == null)
-            {
-                // Skapa vatten vid den låsta ytan
-                mainPlane = lockedTransform;
-                waterInstance = Instantiate(waterPrefab, mainPlane.position, Quaternion.identity);
-                waterInstance.transform.SetParent(mainPlane);
-
-                Debug.Log("Vatten skapat och kopplat till huvudytan.");
-            }
-            else
-            {
-                // Uppdatera vattnets position och storlek om AR-yta förändras
-                waterInstance.transform.position = mainPlane.position;
-                AdjustWaterSize();
-            }
-        }
-        else
-        {
-            Debug.Log("Ingen låst yta hittades, vattnet kommer inte att skapas än.");
-        }
-    }
-
-    void AdjustWaterSize()
-    {
-        if (drivingSurfaceManager.LockedPlane != null && waterInstance != null)
-        {
-            ARPlane plane = drivingSurfaceManager.LockedPlane.GetComponent<ARPlane>();
-            if (plane != null)
-            {
-                // Justera vattnets storlek till AR-ytans storlek
-                waterInstance.transform.localScale = new Vector3(plane.size.x, 1, plane.size.y);
-                Debug.Log($"Vatten uppdaterat till storlek: {plane.size.x} x {plane.size.y}");
-            }
-        }
-    }
-}
--- Assets/Starter Package/ReticleBehaviour.cs
+++ Assets/Starter Package/ReticleBehaviour.cs
@@ -14,56 +14,57 @@
  * limitations under the License.
  */
 
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
+     using System.Linq;
 
-using Unity.Collections;
-using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
+    using Unity.Collections;
+    using UnityEngine;
+    using UnityEngine.XR.ARFoundation;
+    using UnityEngine.XR.ARSubsystems;
 
-public class ReticleBehaviour : MonoBehaviour
-{
-    public GameObject Child;
-    public DrivingSurfaceManager DrivingSurfaceManager;
-
-    public ARPlane CurrentPlane;
-
-    // Start is called before the first frame update
-    private void Start()
+    public class ReticleBehaviour : MonoBehaviour
     {
-        Child = transform.GetChild(0).gameObject;
-    }
+        public GameObject Child;
+        public DrivingSurfaceManager DrivingSurfaceManager;
 
-    private void Update()
-    {
-        // TODO: Conduct a ray cast to position this object.
+        public ARPlane CurrentPlane;
 
-        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-
-        var hits = new List<ARRaycastHit>();
-        DrivingSurfaceManager.RaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinBounds); 
-
-        CurrentPlane = null;
-        ARRaycastHit? hit = null;
-        if (hits.Count > 0)
+        // Start is called before the first frame update
+        private void Start()
         {
-         // If you don't have a locked plane already...
-        var lockedPlane = DrivingSurfaceManager.LockedPlane;
-        hit = lockedPlane == null
+            Child = transform.GetChild(0).gameObject;
+        }
+
+        private void Update()
+        {
+            // TODO: Conduct a ray cast to position this object.
+
+            var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+
+            var hits = new List<ARRaycastHit>();
+DrivingSurfaceManager.RaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinBounds);
+
+            CurrentPlane = null;
+ARRaycastHit? hit = null;
+if (hits.Count > 0)
+{
+    // If you don't have a locked plane already...
+    var lockedPlane = DrivingSurfaceManager.LockedPlane;
+    hit = lockedPlane == null
         // ... use the first hit in `hits`.
         ? hits[0]
         // Otherwise use the locked plane, if it's there.
         : hits.SingleOrDefault(x => x.trackableId == lockedPlane.trackableId);
-        }
-
-        if (hit.HasValue)
+}
+ 
+ if (hit.HasValue)
 {
     CurrentPlane = DrivingSurfaceManager.PlaneManager.GetPlane(hit.Value.trackableId);
     // Move this reticle to the location of the hit.
     transform.position = hit.Value.pose.position;
-}       
-        Child.SetActive(CurrentPlane != null);
+}
+Child.SetActive(CurrentPlane != null);
+
     }
 }
